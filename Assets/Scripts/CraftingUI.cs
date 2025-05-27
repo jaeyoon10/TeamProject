50851ce069a5bee5564ea0495bab75ad49f0cfdf
jaeyoon10 <willis104@naver.com>
2025-05-28 00:06:21 +0900

    UI
    UI

---
+++ Assets/Scripts/CraftingUI.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CraftingUI : MonoBehaviour
+{
+    public static CraftingUI Instance { get; private set; }
+
+    [Header("간단 리스트")]
+    public GameObject recipeScrollView;     // RecipeScrollView 루트 오브젝트
+    public Transform content;              // Content(=슬롯Container) Transform
+    public RecipeSlot slotPrefab;           // RecipeSlot 프리팹
+    public RecipeData[] allRecipes;         // 만들어둔 SO 배열
+
+    [Header("상세 뷰")]
+    public GameObject detailPanel;     // RecipeDetailPanel
+    public Image detailIcon;
+    public TMP_Text detailName;
+    public TMP_Text levelText;
+    public Transform ingContainer;    // IngContainer
+    public GameObject ingPrefab;       // IngItem 프리팹
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+
+        // 시작 시 두 패널 다 끄기
+        recipeScrollView.SetActive(false);
+        detailPanel.SetActive(false);
+    }
+
+    // 1) “제작” 버튼에 연결
+    public void ShowList()
+    {
+        recipeScrollView.SetActive(true);
+
+        // 기존 슬롯 지우기
+        foreach (Transform t in content) Destroy(t.gameObject);
+
+        // SO만큼 슬롯 생성
+        foreach (var r in allRecipes)
+        {
+            var slot = Instantiate(slotPrefab, content);
+            slot.Init(r);
+        }
+    }
+
+    // 2) RecipeSlot 클릭 시 호출
+    public void ShowDetail(RecipeData rd)
+    {
+        detailPanel.SetActive(true);
+
+        // 이미지·이름·레벨
+        detailIcon.sprite = rd.icon;
+        detailName.text = rd.weaponName;
+        levelText.text = "" + rd.requiredLevel;
+
+        // 재료 컨테이너 초기화
+        foreach (Transform t in ingContainer) Destroy(t.gameObject);
+
+        // 재료 슬롯 생성
+        foreach (var ing in rd.ingredients)
+        {
+            var go = Instantiate(ingPrefab, ingContainer);
+            go.transform.Find("Icon").GetComponent<Image>().sprite =
+                ing.icon;
+            go.transform.Find("CountText").GetComponent<TMP_Text>().text =
+                ing.amount.ToString();
+        }
+    }
+
+    public void CloseDetail()
+    {
+        detailPanel.SetActive(false);
+    }
+
+    public void CloseList()
+    {
+        recipeScrollView.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/RecipeData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "NewRecipeData", menuName = "Crafting/Recipe Data")]
+public class RecipeData : ScriptableObject
+{
+    public string weaponName;
+    public Sprite icon;           // 중앙에 크게 띄울 이미지
+    public int requiredLevel;     // Lv 텍스트용
+
+    [System.Serializable]
+    public struct Ingredient
+    {
+        public Sprite icon;
+        public int amount;
+    }
+
+    public List<Ingredient> ingredients = new List<Ingredient>();
+}
