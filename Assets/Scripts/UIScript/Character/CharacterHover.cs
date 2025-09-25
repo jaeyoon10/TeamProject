6e147da93dea967c6dfbd9ba5be4e331abde4be2
QWER_236478 <acy021589@gmail.com>
2025-09-25 16:14:29 +0900

    ji
    juju

--- Assets/Scripts/UIScript/Character/CharacterHover.cs
+++ Assets/Scripts/UIScript/Character/CharacterHover.cs
@@ -1,24 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Image 컴포넌트를 사용하기 위해 추가해야 합니다.
 using UnityEngine.EventSystems;
 
+// 이 스크립트가 Image 컴포넌트를 가진 게임 오브젝트에만 붙도록 강제합니다.
+[RequireComponent(typeof(Image))]
 public class CharacterHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    private Vector3 originalScale;
+    private Image image;
+    private Color originalColor = new Color(133f / 255f, 133f / 255f, 133f / 255f);
+    private Color hoverColor = Color.white; // new Color(1f, 1f, 1f)와 동일합니다.
 
     private void Start()
     {
-        originalScale = transform.localScale;
+        // 스크립트가 붙어있는 게임 오브젝트에서 Image 컴포넌트를 가져옵니다.
+        image = GetComponent<Image>();
+
+        // 시작할 때 기본 색상으로 설정합니다.
+        image.color = originalColor;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        transform.localScale = originalScale * 1.1f; // 살짝 커짐
+        // 마우스 커서를 올렸을 때 색상을 변경합니다.
+        image.color = hoverColor;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        transform.localScale = originalScale; // 원래대로
+        // 마우스 커서가 벗어났을 때 원래 색상으로 되돌립니다.
+        image.color = originalColor;
     }
 }
